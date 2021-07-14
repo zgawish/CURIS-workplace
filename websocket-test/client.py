40a978f4dd01f79236162fe789fcd0f6429c0e8e
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-07-14 15:45:56 -0700

    socket files

---
+++ websocket-test/client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 1024
+PORT = 5050
+SERVER = "192.168.86.23" # IP of head node
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISCONNECT"
+REQUEST_MSG = "!REQUEST"
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello Everyone!")
+input()
+send("Hello Z!")
+input()
+send(REQUEST_MSG)
+input()
+send(DISCONNECT_MSG)
