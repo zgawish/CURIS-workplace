3ab204a1ac9fae7166224d4a62c2c2803ef260fc
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-07-19 09:13:04 -0700

    dummy client

---
+++ websocket-test/client_poll.py
@@ -0,0 +1,34 @@
+import socket
+import time
+
+HEADER = 1024
+PORT = 5050
+SERVER = "10.128.0.3" # internal IP of head node
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISCONNECT"
+REQUEST_MSG = "!REQUEST"
+
+start = time.time()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+end = time.time()
+print("Time elapsed: " + str(end - start))
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+start = time.time()
+for i in range(1000):
+    send('Hello')
+time.sleep(10)    
+end = time.time()
+print("Time elapsed: " + str(end - start))
