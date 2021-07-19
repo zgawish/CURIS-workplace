43941e16a2c0e57abbb6faeac18768087330eca5
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-07-19 09:26:05 -0700

    speed test

---
+++ websocket-test/client_timed.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+HEADER = 1024
+PORT = 5050
+SERVER = "10.128.0.3" # internal IP of head node
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISCONNECT"
+REQUEST_MSG = "!REQUEST"
+
+start = time.time()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+end = time.time()
+print("Time elapsed: " + str(end - start))
+
+
+def send(msg):
+    start = time.time()
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+    end = time.time()
+    print("Time elapsed: " + str(end - start))
+
+
+msg = input()
+while msg != "": #cmd to run a command
+    send(msg)
+    msg = input()
+send(DISCONNECT_MSG)
